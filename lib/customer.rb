d42f39a48d27540d9807f550704f355493d4deb9
Ada Lee <lili545442483@163.com>
2016-01-24 18:22:56 +0800

    create new class Customer, add DuplicateCustomerError, uncomment customer part in app.rb

---
+++ lib/customer.rb
@@ -0,0 +1,25 @@
+class Customer
+  attr_reader :name
+
+  @@customers = []
+
+  def initialize(options={})
+    @name = options[:name]
+    add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+
+  def self.find_by_name(name)
+    @@customers.select { |customer| customer.name == name }.first
+  end
+
+  def add_to_customers
+    if @@customers.map { |customer| customer.name }.include? @name
+      raise DuplicateCustomerError, "'#{@name}' already exists!"
+    end
+    @@customers << self
+  end
+end
