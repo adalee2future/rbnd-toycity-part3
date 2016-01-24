3740daba8b8c493661a788ad8728f7797d203d4a
Ada Lee <lili545442483@163.com>
2016-01-24 17:42:40 +0800

    create and handle DuplicateProductError

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,23 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
-  def initialize(info)
-    @@products << self
+  def initialize(options={})
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    if @@products.map(&:title).include? @title
+      raise DuplicateProductError, "'#{@title}' already exists!"
+    end
+    @@products << self
   end
 end
